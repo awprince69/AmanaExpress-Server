029cb0a66f74efacc2a51f279b1eaa09b42c3bb4
awprince69 <kali.nslookup@gmail.com>
2021-04-02 17:49:42 +0600

    node

--- index.js
+++ index.js
@@ -18,11 +18,12 @@ app.get('/', (req, res) => {
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kmkzt.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
+    console.log(err);
     const collection = client.db("amanaExpress").collection("products");
     const orderCollection = client.db("amanaExpress").collection("orders");
 
     app.get('/products', (req, res) => {
-        collection.find()
+        collection.find({})
             .toArray((err, product) => {
                 res.send(product);
             })
